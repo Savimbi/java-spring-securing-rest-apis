890e8071631a549f756846132fc1ff8d249fed94
Dukuzumuremyi Dominique | PPD <ts-dom.dukuzumuremyi@rakuten.com>
2021-05-10 22:14:48 +0900

    Completed module

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -8,9 +8,12 @@ import java.util.UUID;
 @Component
 public class ResolutionInitializer implements SmartInitializingSingleton {
 	private final ResolutionRepository resolutions;
+	private final UserRepository users;
+	private static final String GENERAL_PASSWORD = "{bcrypt}$2a$10$LDI1pbUjS.flx7jUMM3J..uq5woM5SYGIIlb7PEGFIbN.wPgcK89K";
 
-	public ResolutionInitializer(ResolutionRepository resolutions) {
+	public ResolutionInitializer(ResolutionRepository resolutions, UserRepository users) {
 		this.resolutions = resolutions;
+		this.users = users;
 	}
 
 	@Override
@@ -18,5 +21,27 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
 		this.resolutions.save(new Resolution("Read War and Peace", "user"));
 		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
 		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+
+		User user = new User("user",GENERAL_PASSWORD);
+		user.grantAuthority("resolution:read");
+		user.grantAuthority("resolution:write");
+		this.users.save(user);
+
+		User hasread = new User();
+		hasread.setId();
+		hasread.setUsername("hasread");
+		hasread.setPassword(GENERAL_PASSWORD);
+		hasread.grantAuthority("resolution:read");
+		this.users.save(hasread);
+
+		User haswrite = new User();
+		haswrite.setId();
+		haswrite.setUsername("haswrite");
+		haswrite.setPassword(GENERAL_PASSWORD);
+		haswrite.grantAuthority("resolution:write");
+		this.users.save(haswrite);
+
 	}
+
+
 }
